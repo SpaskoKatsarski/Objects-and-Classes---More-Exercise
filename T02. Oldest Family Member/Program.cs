aef279aa84779f38912a55b5baca2273679ed05c
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-03-01 23:05:22 +0200

    Add project files.

---
+++ T02. Oldest Family Member/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace T02._Oldest_Family_Member
+{
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> allPeople = new List<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split(' ');
+
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                Person currentPerson = new Person(name, age);
+
+                allPeople.Add(currentPerson);
+            }
+
+            Person oldestPerson = allPeople.OrderByDescending(p => p.Age).First();
+
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+        }
+    }
+}
